1b68f8c91038a99870a396f31ef0a7124cad3f1c
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-29 00:59:03 +0100

    first api calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
 async function getResults(query) {
-  axios(`https://www.food2fork.com/api/search`)
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = 'd6a42255d7c3795a2f2630193c48accf';
+
+  try {
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);  
+  } catch (error) {
+    alert(error);
+  }
 }
 
-getResults();
+getResults('pizza');
