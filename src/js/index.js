c3d5deade476933e22e075575a6cc9432bf29088
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-29 01:14:58 +0100

    search model build

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const key = 'd6a42255d7c3795a2f2630193c48accf';
-
-  try {
-    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);  
-  } catch (error) {
-    alert(error);
-  }
-}
-
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am exported string!';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'd6a42255d7c3795a2f2630193c48accf';
+  
+    try {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      // console.log(this.result);  
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
