341b8f1edcc9dcb784c02bd1133cafd26576e69d
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-04-08 17:59:39 +0200

    config airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": ["prettier", "airbnb-base"],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: ['prettier', 'airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "rules": {
-  }
+  rules: {},
 };
